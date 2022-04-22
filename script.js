7efff3cfb259f5cd4835f027c859075c218364aa
Abid <58351011+abidkakkur11@users.noreply.github.com>
2022-04-22 21:20:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ weather.temperature = {
 }
 
 const KELVIN = 273;
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = "c3426018e98dc7587cf82c683becffcb";
 
 
 if('geolocation' in navigator){
